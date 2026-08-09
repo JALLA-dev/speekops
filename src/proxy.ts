ba03d3b6d0734eb47dfea4fbf1af69c9116c85eb
JALLA-dev <sivananda.jalla@gmail.com>
2026-08-09 11:27:11 +0530

    Export clerkMiddleware(options) directly with publishableKey and secretKey options

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,15 +1,13 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
-if (!process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY) {
-  process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY =
-    'pk_test_cG9zc2libGUtbGFiLTQ1LmNsZXJrLmFjY291bnRzLmRldiQ'
-}
-if (!process.env.CLERK_SECRET_KEY) {
-  process.env.CLERK_SECRET_KEY =
-    'sk_test_wzV1o5gwf5XNJD9iABdhYyXtgstC5Ycyp2us0uRZzj'
-}
-
-export default clerkMiddleware()
+export default clerkMiddleware({
+  publishableKey:
+    process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY ||
+    'pk_test_cG9zc2libGUtbGFiLTQ1LmNsZXJrLmFjY291bnRzLmRldiQ',
+  secretKey:
+    process.env.CLERK_SECRET_KEY ||
+    'sk_test_wzV1o5gwf5XNJD9iABdhYyXtgstC5Ycyp2us0uRZzj',
+})
 
 export const config = {
   matcher: [
